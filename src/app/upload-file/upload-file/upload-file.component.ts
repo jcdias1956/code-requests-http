12ba988ddd7f90596fbbf986bcd2d35a0ecb08c1
José Carlos Dias <jcdias1956@gmail.com>
2019-08-15 19:03:09 -0300

    feature/curso-angular-140-commit

--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -2,6 +2,7 @@ import { environment } from './../../../environments/environment';
 import { Component, OnInit } from '@angular/core';
 import { UploadFileService } from '../upload-file.service';
 import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { filterResponse, uploadProgress } from 'src/app/shared/rxjs-operators';
 
 @Component({
   selector: 'app-upload-file',
@@ -35,17 +36,24 @@ export class UploadFileComponent implements OnInit {
     if (this.files && this.files.size > 0) {
       this.service
         .upload(this.files, environment.BASE_URL + '/uploads')
-        .subscribe((event: HttpEvent<Object>) => {
-          // HttpEventType
-          console.log(event);
-          if (event.type === HttpEventType.Response) {
-            console.log('Upload Concluido');
-          } else if (event.type === HttpEventType.UploadProgress) {
-            const persentDone = Math.round((event.loaded * 100) / event.total);
-            console.log('Progresso', persentDone);
-            this.progress = persentDone;
-          }
-        });
+        .pipe(
+          uploadProgress(progress => {
+            console.log(progress);
+            this.progress = progress;
+          }),
+          filterResponse()
+        )
+        .subscribe(response => console.log('Upload Concluido'));
+        // .subscribe((event: HttpEvent<Object>) => {
+        //   // console.log(event);
+        //   if (event.type === HttpEventType.Response) {
+        //     console.log('Upload Concluido');
+        //   } else if (event.type === HttpEventType.UploadProgress) {
+        //     const persentDone = Math.round((event.loaded * 100) / event.total);
+        //     // console.log('Progresso', persentDone);
+        //     this.progress = persentDone;
+        //   }
+        // });
     }
   }
 }
